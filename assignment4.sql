3fac9ed3b45947944214df5c2c11cf4cae4616c0
Ramesh S <42843515+lonelystag@users.noreply.github.com>
2019-02-18 14:13:16 +0530

    Update assignment4.sql

--- assignment4.sql
+++ assignment4.sql
@@ -2,6 +2,7 @@ create database Company;
 use Company;
 create table Employee(
   name char(20),
-  emp_age int(8));
-alter table employee
-  add primary key(id)
+  emp_age int(8)
+  id int(10)
+  primary key (id));
+
