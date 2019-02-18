05e87e1235877e3191711a3b506385b74e7667a3
Ramesh S <42843515+lonelystag@users.noreply.github.com>
2019-02-18 12:09:53 +0530

    Create assignment8.sql

---
+++ assignment8.sql
@@ -0,0 +1,10 @@
+create database guvi;
+use guvi;
+create table Employee(
+  ID varchar(10) autoincrement,
+  NAME char(15),
+  AGE int(10));
+insert into Employee(ID,NAME,AGE)
+  values('nx1','ramesh','19');
+insert into Employee(ID,NAME,AGE)
+  values('nxc3','javed','18');
